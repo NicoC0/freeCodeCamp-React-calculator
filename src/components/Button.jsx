34c59db29715f0aaf9a06de8870722454480f069
NicoC0 <nicoconfaa@gmail.com>
2022-04-11 02:38:36 -0300

    code optimization

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,13 +6,20 @@ const Button = (props) => {
     return isNaN(valor) && (valor !== ".") && (valor !== "=")
   }
 
-  return (
-    <div className={`boton-contenedor ${esOperador(props.children) ? "operador" : "" }`.trimEnd() }
-    onClick={() => props.manejarClic(props.children)}
-    >
+  if (esOperador(props.children)) {
+    return (
+    <div className="boton-contenedor operador" onClick={() => props.manejarClic(props.children)}>
       {props.children}
     </div>
-  )
-}
+    )
+  } else {
+    return (
+      <div className="boton-contenedor" onClick={() => props.manejarClic(props.children)}>
+        {props.children}
+      </div>
+      )
+    }
+  }
+
 
 export default Button
